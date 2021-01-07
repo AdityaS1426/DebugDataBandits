44e15329b68ade7800d4c36f4ac962055963862e
Atharva Kudrimoti <72766021+athkud123@users.noreply.github.com>
2021-01-06 23:32:08 -0800

    Create GradingSystemRough.java

---
+++ src/GradingSystemRough.java
@@ -0,0 +1,42 @@
+ import java.util.Scanner;
+
+public class GradingSystemRough
+{
+    public static void main(String args[])
+    {
+        int marks[] = new int[4];
+        int i;
+        float total=0, avg;
+        Scanner scanner = new Scanner(System.in);
+		
+        
+        for(i=0; i<4; i++) { 
+           System.out.print("Enter Marks of Test"+(i+1)+":");
+           marks[i] = scanner.nextInt();
+           total = total + marks[i];
+        }
+        scanner.close();
+        avg = total/4;
+        System.out.print("The student's average grade is:");
+        if(avg>=90)
+        {
+            System.out.print("A");
+        }
+        else if(avg>=80 && avg<90)
+        {
+           System.out.print("B");
+        } 
+        else if(avg>=70 && avg<80)
+        {
+            System.out.print("C");
+        }
+        else if(avg>=60 && avg<70)
+        {
+            System.out.print("D");
+        }
+        else {
+          System.out.print("F");
+        }
+    
+    } 
+}
